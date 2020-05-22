ba2f9c796cb97737ae38e5621ea9d419ee97e356
Vladyslav Sidikov <vlad.sidikov007@gmai.com>
2020-05-22 19:55:20 +0300

    without redux

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,53 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
-// import { GetStaticProps } from 'next'
-// import { postsData, Post } from '../utils/posts-data'
+import { GetStaticProps } from 'next'
+import { postsData, Post } from '../utils/posts-data'
 import Layout from '../components/Layout'
 import List from '../components/List'
-import { connect } from 'react-redux'
-import { getPosts } from '../store/actions/actions'
-import { bindActionCreators } from 'redux'
-import { wrapper } from '../store'
-import axios from 'axios'
-import { Dispatch } from 'redux'
+// import { connect } from 'react-redux'
+// import { getPosts } from '../store/actions/actions'
+// import { bindActionCreators } from 'redux'
+// import { wrapper } from '../store'
+// import axios from 'axios'
+// import { Dispatch } from 'redux'
 
-const Index: React.FC = (props: any) => {
-  console.log(props)
+type Props = {
+  items: Post[]
+}
+
+const Index: React.FC<Props> = ({ items }) => {
   return (
     <Layout title="Next MVP">
       <h1>Posts</h1>
-      {/* <List items={data} /> */}
+      <List items={items} />
     </Layout>
   )
 }
 
-export const getStaticProps = wrapper.getStaticProps(async ({ store }) => {
-  store.dispatch(getPosts)
-})
-
-const mapDispatchToProps = (dispatch: Dispatch) => {
-  return {
-    data: bindActionCreators(getPosts, dispatch),
-  }
+export const getStaticProps: GetStaticProps = async () => {
+  const items: Post[] = postsData
+  return { props: { items } }
 }
 
-export default connect(null, mapDispatchToProps)(Index)
+export default Index
 
-// export const getgddg = async () => {
-//   try {
-//     const payload: any = await axios.get('https://simple-blog-api.crew.red/posts')
-//     if (payload) {
-//       console.log(payload.data)
-//     }
-//   } catch (error) {
-//     console.log(error.message)
+// export const getStaticProps = wrapper.getStaticProps(async ({ store }) => {
+//   store.dispatch(getPosts)
+// })
+
+// const mapDispatchToProps = (dispatch: Dispatch) => {
+//   return {
+//     data: bindActionCreators(getPosts, dispatch),
 //   }
 // }
 
-// getgddg()
+// export default connect(null, mapDispatchToProps)(Index)
+
+// export const getPost = async () => {
+//   return await axios
+//     .get('https://simple-blog-api.crew.red/posts')
+//     .then((response) => console.log(response.data))
+//     .catch((error) => console.log(error.message))
+// }
+
+// getPost()
